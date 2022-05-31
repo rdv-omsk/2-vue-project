e85beef27a8eda1a1467c3f93de07debf71293f6
rdv-omsk <rdv-omsk@mail.ru>
2022-05-31 11:31:34 +0300

    9

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list
+          subheader
+          two-line
+        >
+          <v-list-tile
+            avatar
+            v-for="order in orders"
+            :key="order.id"
+          ><v-list-tile-action>
+              <v-checkbox
+                color="success"
+                :input-value="order.done"
+                @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                :to="'/ad/' + order.adId"
+                class="primary"
+              >Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +40,20 @@
 export default {
   data () {
     return {
-
+      orders: [
+        {
+          id: 'ord1',
+          name: 'Dmitry',
+          phone: '8-913-640-10-10',
+          adId: '123',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
     }
   }
 }
