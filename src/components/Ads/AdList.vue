ccd7c16af3f8de586985f0c5cb4202343f5cd5e8
rdv-omsk <rdv-omsk@mail.ru>
2022-05-31 15:09:30 +0300

    11

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My ads</h1>
+
+        <v-card
+          class="elevation-10 mb-3"
+          v-for="ad in ads"
+          :key="ad.id"
+        >
+          <v-layout row>
+            <v-flex xs4>
+              <v-card-media
+                :src="ad.imageSrc"
+                height="160px"
+              ></v-card-media>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ ad.title }}</h2>
+                <p>{{ ad.description }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="info"
+                  :to="'/ad/' + ad.id"
+                >Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +40,36 @@
 export default {
   data () {
     return {
-
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Hello, i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          id: '123'
+        },
+        {
+          title: 'Second ad',
+          description: 'Hello, i am description',
+          promo: true,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          id: '124'
+        },
+        {
+          title: 'Third ad',
+          description: 'Hello, i am description',
+          promo: true,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          id: '125'
+        },
+        {
+          title: 'Fourth ad',
+          description: 'Hello, i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '126'
+        }
+      ]
     }
   }
 }
